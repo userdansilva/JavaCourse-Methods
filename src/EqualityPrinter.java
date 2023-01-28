8855b6cc526c22cca27478e0c89d2b639f39a049
Daniel Silva <daniel.sousasilva97@outlook.com>
2023-01-28 06:59:48 -0300

    EqualityPrinter done!

---
+++ src/EqualityPrinter.java
@@ -0,0 +1,27 @@
+public class EqualityPrinter {
+    public static void main(String[] args) {
+        printEqual(1, 1, 1);
+        printEqual(1, 1, 2);
+        printEqual(-1, -1, -1);
+        printEqual(1, 2, 3);
+    }
+
+    public static void printEqual(int x, int y, int z) {
+        if (x < 0 | y < 0 | z < 0) {
+            System.out.println("Invalid Value");
+            return;
+        }
+
+        if ((x == y) & (y == z)) {
+            System.out.println("All numbers are equal");
+            return;
+        }
+
+        if (x != y && y != z && x != z) {
+            System.out.println("All numbers are different");
+            return;
+        }
+
+        System.out.println("Neither all are equal or different");
+    }
+}
